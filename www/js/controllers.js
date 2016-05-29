d4bb15b349be89e515cc2e1df4c5de03bafe82fd
Anthony Li <anthli1895@gmail.com>
2016-05-29 02:57:19 -0400

    Added .gitignore and .bowerrc

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,11 @@
 var app = angular.module('ratsValidator.controllers', []);
 
 app.controller('DashCtrl', function($scope) {
-	$scope.doRefresh = function() {
+  $scope.doRefresh = function() {
    // here refresh data code
-	   $scope.$broadcast('scroll.refreshComplete');
-	   $scope.$apply()
-	};
+     $scope.$broadcast('scroll.refreshComplete');
+     $scope.$apply()
+  };
 });
+
 app.controller('newPostCtrl', function($scope) {});
